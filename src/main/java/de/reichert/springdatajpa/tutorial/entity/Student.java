3ea0b986dbf70556b61f6464d7ef62d32a13e8e2
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-01-04 19:08:32 +0100

    H2 Datenbank konfiguriert

---
+++ src/main/java/de/reichert/springdatajpa/tutorial/entity/Student.java
@@ -0,0 +1,7 @@
+package de.reichert.springdatajpa.tutorial.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Student {
+}
---
+++ src/test/java/de/reichert/springdatajpa/tutorial/SpringDataJpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package de.reichert.springdatajpa.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
